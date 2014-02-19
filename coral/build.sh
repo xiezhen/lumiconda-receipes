d73b0e220ca4292e5a8d6e2d239e880387749aff
Zhen Xie <zhen@pczhen.cern.ch>
2014-02-19 15:13:00 +0100

    build coral

--- coral/build.sh
+++ coral/build.sh
@@ -4,8 +4,9 @@ ${SRC_DIR}/buildcoral.sh CORAL_2_3_28
 
 cd ${SRC_DIR}/CORAL_2_3_28
 
-make ORACLEINC="${PREFIX}/include" ORACLELIB="${PREFIX}/lib" XERCESINC="${PREFIX}/include/xercesc" XERCESLIB="${PREFIX}/lib" SQLITEINC="${PREFIX}/include" SQLITELIB="${PREFIX}/lib" FRONTIERINC="${PREFIX}/include" FRONTIERLIB="${PREFIX}/lib"  
+make ORACLEINC="${PREFIX}/include" ORACLELIB="${PREFIX}/lib" XERCESINC="${PREFIX}/include" XERCESLIB="${PREFIX}/lib" SQLITEINC="${PREFIX}/include" SQLITELIB="${PREFIX}/lib" FRONTIERINC="${PREFIX}/include" FRONTIERLIB="${PREFIX}/lib" PYTHONINC="${PREFIX}/include/python${PY_VER}" PYTHONLIB="${PREFIX}/lib"
 make PREFIX="${PREFIX}" install
+mv ${PREFIX}/lib/coral.py ${PREFIX}/lib/python${PY_VER}/
 
 # See
 # http://docs.continuum.io/conda/build.html
