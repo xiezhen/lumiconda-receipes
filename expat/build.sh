981a4a2e6f1fca7a15014c688d3b4fda77b34e39
Zhen Xie <zhen@pczhen.cern.ch>
2014-02-24 12:39:26 +0100

    rm extra dirs

--- expat/build.sh
+++ expat/build.sh
@@ -4,6 +4,9 @@
 make
 make install
 
+rm -rf ${PREFIX}/man
+rm -rf ${PREFIX}/bin
+
 # See
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
