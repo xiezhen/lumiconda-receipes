02ccf621a108d7f6c5b530e7e151addcc42b8d65
Zhen Xie <zhen@pczhen.cern.ch>
2014-02-24 12:40:17 +0100

    remove extra dirs

--- xz/build.sh
+++ xz/build.sh
@@ -3,6 +3,8 @@
 ./configure CC=gcc CFLAGS='-fPIC -O2' --prefix=${PREFIX} --disable-static
 make
 make install
+rm -rf ${PREFIX}/share
+rm -rf ${PREFIX}/bin
 
 # See
 # http://docs.continuum.io/conda/build.html
