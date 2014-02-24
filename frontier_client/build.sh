7394f2cecdc25da51e3a26da64b5d5e419baa487
Zhen Xie <zhen@pczhen.cern.ch>
2014-02-24 12:39:41 +0100

    rm extra dirs

--- frontier_client/build.sh
+++ frontier_client/build.sh
@@ -4,6 +4,8 @@ GCCVERSION=`gcc -dumpversion`
 
 make EXPAT_DIR="${PREFIX}" ZLIB_DIR="${PREFIX}" PACPARSER_DIR="${PREFIX}" COMPILER_TAG="gcc_${GCCVERSION}" distdir="${PREFIX}" dist
 
+rm ${PREFIX}/RELEASE_NOTES
+
 # See
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
