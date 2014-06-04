f3de8b89b7cb1e2911fd50179dddd9a590bf1c86
Zhen Xie <zhen@pczhen.cern.ch>
2014-06-04 16:45:35 +0200

    change tag version

--- coral/build.sh
+++ coral/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-${SRC_DIR}/buildcoral.sh CORAL_2_4_1
+${SRC_DIR}/buildcoral.sh CORAL_2_4_2
 
-cd ${SRC_DIR}/CORAL_2_4_1
+cd ${SRC_DIR}/CORAL_2_4_2
 
-make -j8 BOOSTINC="${PREFIX}/include" BOOSTLIB="${PREFIX}/lib" ORACLEINC="${PREFIX}/include" ORACLELIB="${PREFIX}/lib" XERCESINC="${PREFIX}/include" XERCESLIB="${PREFIX}/lib" SQLITEINC="${PREFIX}/include" SQLITELIB="${PREFIX}/lib" FRONTIERINC="${PREFIX}/include" FRONTIERLIB="${PREFIX}/lib" PYTHONINC="${PREFIX}/include/python${PY_VER}" PYTHONLIB="${PREFIX}/lib" 
+make -j2 BOOSTINC="${PREFIX}/include" BOOSTLIB="${PREFIX}/lib" ORACLEINC="${PREFIX}/include" ORACLELIB="${PREFIX}/lib" XERCESINC="${PREFIX}/include" XERCESLIB="${PREFIX}/lib" SQLITEINC="${PREFIX}/include" SQLITELIB="${PREFIX}/lib" FRONTIERINC="${PREFIX}/include" FRONTIERLIB="${PREFIX}/lib" PYTHONINC="${PREFIX}/include/python${PY_VER}" PYTHONLIB="${PREFIX}/lib" 
 make PREFIX="${PREFIX}" install
 mv ${PREFIX}/lib/coral.py ${PREFIX}/lib/python${PY_VER}/
 cd $PREFIX/lib/python${PY_VER}
