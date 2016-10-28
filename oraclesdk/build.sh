24243f4c1f75527739f17e3c26aefc34b0089115
Zhen Xie <zhen@pczhen.cern.ch>
2016-10-28 21:48:58 +0200

    remove unused

--- oraclesdk/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -vp  ${PREFIX}/include
-
-cp -r ${SRC_DIR}/sdk/include/. ${PREFIX}/include
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
